cb30e7ce93ba614008d34b2dd93e287b3f6de58f
saulo775 <59702227+saulo775@users.noreply.github.com>
2020-11-10 11:40:58 -0300

    correção na criação de transação

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -33,7 +33,7 @@ transactionRouter.post('/', (request, response) => {
       type,
     });
 
-    return response.json({ transaction });
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
